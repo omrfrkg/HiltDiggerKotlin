0e8a0497b1dfa0fa9b9bf094e6d3334ce793db68
Ömer Faruk Gündoğdu <omerrfarukgundogdu@gmail.com>
2024-08-17 16:53:30 +0300

    Dagger - Hilt çalışması için proje dosyaları oluşturuldu.

---
+++ app/src/main/java/com/example/hiltkotlin/Instrument.kt
@@ -0,0 +1,4 @@
+package com.example.hiltkotlin
+
+class Instrument {
+}
---
+++ app/src/main/java/com/example/hiltkotlin/Musician.kt
@@ -0,0 +1,7 @@
+package com.example.hiltkotlin
+
+class Musician(instrument: Instrument,band: Band) {
+    fun sing(){
+        println("working..")
+    }
+}
